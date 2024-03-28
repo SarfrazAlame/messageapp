b6506faed4d54ec1d587618f90fcef072f1c5a7e
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-03-28 11:03:19 +0530

    sdlkhfn

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,17 @@
 import GoogleProvider from "next-auth/providers/google"
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter"
+import { db } from "./db"
+import { NextAuthOptions } from "next-auth"
+import { Adapter } from "next-auth/adapters"
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+    adapter: UpstashRedisAdapter(db) as Adapter,
+    session: {
+        strategy: "jwt"
+    },
+    pages: {
+        signIn: '/login'
+    },
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
